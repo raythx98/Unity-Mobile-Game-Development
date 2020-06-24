f81e91b2a0eaab0763a77e50f0d854ea51e2a8ed
g-erm <tanyuting.gt@gmail.com>
2020-06-24 16:41:56 +0800

    i cried while doing this
    jk

---
+++ Assets/Germs/Scripts/CoinCollector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CoinCollector : MonoBehaviour
+{
+    private float noOfCoins = 0f;
+
+    public TextMeshProUGUI coinsText;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.transform.tag == "item") {
+            noOfCoins ++;
+            coinsText.text = noOfCoins.ToString();
+
+            Destroy(other.gameObject);
+        }
+    }
+}
--- Assets/Germs/Scripts/ItemSensor.cs
+++ Assets/Germs/Scripts/ItemSensor.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySensor : MonoBehaviour
+public class ItemSensor : MonoBehaviour
 {
     // Update is called once per frame
     void Update()
     {
-        if (GameObject.FindGameObjectsWithTag("enemy").Length == 0) 
+        if (GameObject.FindGameObjectsWithTag("item").Length == 0) 
         {
             GameObject.Find("gameCanvas").GetComponent<GameManager>().WinGame();
         }
